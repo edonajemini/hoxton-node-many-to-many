11a0bb2b39666ae1670f8d58590ec21e2c96811b
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-09-08 19:16:19 +0200

    Finished

--- index.ts
+++ index.ts
@@ -25,6 +25,9 @@ app.get('/', (req, res) => {
   const getInterviewers = db.prepare(`
   SELECT * FROM interviewers;
   `)
+  const getInterviews = db.prepare(`
+  SELECT * FROM interviewers;
+  `)
   
   const getApplicantsById = db.prepare(`
   SELECT * FROM applicants WHERE id = ?;
@@ -32,8 +35,14 @@ app.get('/', (req, res) => {
   const getInterviewersById = db.prepare(`
 SELECT * FROM interviewers WHERE id = ?;
 `)
+const getInterviewsById = db.prepare(`
+SELECT * FROM interviews WHERE id = ?;
+`)
   const getInterviewsForApplicants = db.prepare(`
   SELECT * FROM interviews WHERE applicantsId = ?;
+  `)
+  const getInterviewsForInterviewers = db.prepare(`
+  SELECT * FROM interviews WHERE interviewersId = ?;
   `)
   const getInterviewersForApplicants = db.prepare(`
   SELECT interviewers.* FROM interviewers
@@ -47,12 +56,12 @@ WHERE interviews.applicantsId = ?;
 `)
   
 
-//get applicants 
+//get applicants with their interviews and interviewers
   app.get('/applicants', (req, res) => {
     const applicants = getApplicants.all()
     for(let applicant of applicants){
-        const interviewers = getInterviewersForApplicants.all(applicant.id)
-        applicant.interviewers = interviewers
+        applicant.interviews = getInterviewsForApplicants.all(applicant.id)
+        applicant.interviewers = getInterviewersForApplicants.all(applicant.id)
     }
     res.send(applicants)
   })
@@ -70,12 +79,12 @@ WHERE interviews.applicantsId = ?;
     }
   })
 
-  //get Interviewers
+  //get Interviewers with their interviews and applicants
   app.get('/interviewers', (req, res) => {
       const interviewers = getInterviewers.all()
       for(let interviewer of interviewers){
-        const applicants = getApplicantsForInterviewers.all(interviewer.id)
-        interviewer.applicants = applicants
+        interviewer.interviews = getInterviewsForInterviewers.all(interviewer.id)
+        interviewer.applicants = getApplicantsForInterviewers.all(interviewer.id)
     }
       res.send(interviewers)
   })
@@ -85,6 +94,7 @@ app.get('/interviewers/:id', (req, res) => {
     const id = Number(req.params.id)
   const interviewer = getInterviewersById.get(id)
     if (interviewer) {
+        interviewer.interviews = getInterviewsForInterviewers.all(interviewer.id)
         interviewer.applicants = getApplicantsForInterviewers.all(interviewer.id)
       res.send(interviewer)
     } else {
@@ -92,7 +102,104 @@ app.get('/interviewers/:id', (req, res) => {
     }
   })
 
- 
+  //get Interviews
+  app.get('/interviews', (req, res) => {
+    const interviews = getInterviews.all()
+    res.send(interviews)
+  })
+
+
+  // Post applicants
+  const postApplicant = db.prepare(`
+  INSERT INTO applicants (name, age, position) VALUES (?, ?, ?);
+  `)
+
+  app.post('/applicants', (req, res) => {
+    const name = req.body.name
+    const age = req.body.age
+    const position = req.body.position
+      let errors: string[] = []
+      
+      if (typeof req.body.name !== 'string') {
+          errors.push('Add a proper NAME!')
+        }
+     
+      if(typeof req.body.age  !=='number') {
+          errors.push('Add a proper TYPE OF AGE')
+      }
+      if (typeof req.body.position !== 'string') {
+        errors.push('Add a proper POSITION!')
+      }
+      if( errors.length === 0)  {
+        const applicantInfo = postApplicant.run(name, age, position)
+        const newApplicants = getApplicantsById.get(applicantInfo.lastInsertRowid)
+        res.send(newApplicants)
+      }
+      else {
+          res.status(400).send({ errors: errors })
+        }
+  })
+   // Post Interviewer
+   const postInterviewer = db.prepare(`
+  INSERT INTO interviewers (name, company) VALUES (?, ?);
+  `)
+
+  app.post('/interviewers', (req, res) => {
+    const name = req.body.name
+    const company = req.body.company
+      let errors: string[] = []
+      
+      if (typeof req.body.name !== 'string') {
+          errors.push('Add a proper NAME!')
+        }
+     
+      if(typeof req.body.company  !=='string') {
+          errors.push('Add a proper TYPE OF company')
+      }
+      if( errors.length === 0)  {
+        const interviewerInfo = postInterviewer.run(name, company)
+        const newInterviewer = getInterviewersById.get(interviewerInfo.lastInsertRowid)
+        res.send(newInterviewer)
+      }
+      else {
+          res.status(400).send({ errors: errors })
+        }
+  })
+    // Post Interviews
+    const postInterviews = db.prepare(`
+    INSERT INTO interviews (applicantsId, interviewersId, time, place) VALUES (?, ?, ?, ?);
+    `)
+  
+    app.post('/interviews', (req, res) => {
+      const applicantsId = req.body.applicantsId
+      const interviewersId = req.body.interviewersId
+      const time = req.body.time
+      const place = req.body.place
+        let errors: string[] = []
+        
+        if (typeof req.body.applicantsId !== 'number') {
+            errors.push('Add a proper applicant Id!')
+          }
+        if(typeof req.body.interviewersId  !=='number') {
+            errors.push('Add a proper interviewer Id')
+        }
+        if(typeof req.body.time  !=='string') {
+            errors.push('Add a proper TIME')
+        }
+        if(typeof req.body.place  !=='string') {
+            errors.push('Add a proper place')
+        }
+        if( errors.length === 0)  {
+          const interviewInfo = postInterviews.run(applicantsId, interviewersId, time, place)
+          const newInterviews = getInterviewsById.get(interviewInfo.lastInsertRowid)
+          res.send(newInterviews)
+        }
+        else {
+            res.status(400).send({ errors: errors })
+          }
+    })
+
+
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
